538fe8b49d54f3a555a0704315d0806b87555166
Eylon <eylon.perets@gmail.com>
2024-09-28 21:34:18 +0300

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { ThemeProvider } from '@/components/theme-provider';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { Navbar } from '@/components/navbar';
+import Header from '@/components/Header';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          forcedTheme="dark"
+        >
+          <TooltipProvider>
+            <div className="flex min-h-screen">
+              <Navbar />
+              <main className="flex min-h-screen w-full flex-1 flex-col bg-muted/40">
+                <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                  <div className="px-4 sm:px-6">
+                    <Header />
+                  </div>
+                  <div className="">{children}</div>
+                </div>
+              </main>
+            </div>
+          </TooltipProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
